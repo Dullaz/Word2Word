184637aa0da040e44488c47f41ac10c68cc18e2d
Abdullah Hasan <abdullah.hasan@payaut.com>
2022-09-13 21:12:35 +0200

    basics

---
+++ pages/component/Card.tsx
@@ -0,0 +1,87 @@
+import type { Identifier, XYCoord } from 'dnd-core'
+import type { FC } from 'react'
+import { useRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
+
+import styles from '../../styles/Home.module.css'
+
+
+const ItemTypes = {
+    CARD: 'card',
+  }
+
+const style = {
+  border: '1px dashed gray',
+  padding: '0.5rem 1rem',
+  marginBottom: '.5rem',
+  backgroundColor: 'white',
+  cursor: 'move',
+}
+
+export interface CardProps {
+  id: any
+  text: string
+  index: number
+  moveCard: (dragIndex: number, hoverIndex: number) => void
+}
+
+interface DragItem {
+  index: number
+  id: string
+  type: string
+}
+
+export const Card: FC<CardProps> = ({ id, text, index, moveCard }) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [{ handlerId }, drop] = useDrop<
+    DragItem,
+    void,
+    { handlerId: Identifier | null }
+  >({
+    accept: ItemTypes.CARD,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item: DragItem, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex)
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    },
+  })
+
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.CARD,
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor: any) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+  return (
+    <div ref={ref} style={{ opacity }} data-handler-id={handlerId} className={styles.card}>
+      <h2>{text}</h2>
+    </div>
+  )
+}
---
+++ pages/component/Letter.tsx
@@ -0,0 +1,79 @@
+import React, {useCallback, useState, FC} from "react";
+
+import update from 'immutability-helper'
+import styles from '../../styles/Home.module.css'
+
+import {Card} from './Card'
+
+interface Letter {
+  id: number,
+  text: string
+}
+
+interface WordState {
+  letters: Letter[]
+}
+
+const checkTarget = (cards: Letter[]) => {
+  const target = [3,2,1]
+  console.log(cards.map((card, i) => card.text).toString())
+  for(var card of cards) {
+    if(card.id != target.pop()) { return false }
+  }
+  return true
+}
+
+export const Word: FC = () => {
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      text: 'A',
+    },
+    {
+      id: 2,
+      text: 'B',
+    },
+    {
+      id: 3,
+      text: 'C',
+    }])
+
+    const [isDone, setIsDone] = useState(false);
+
+    const moveCard = useCallback((dragIndex: number, hoverIndex: number) => {
+      setCards((prevCards: Letter[]) => 
+        update(prevCards, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, prevCards[dragIndex] as Letter],
+          ],
+        }),
+      )
+
+    }, [])
+
+    const renderCard = useCallback(
+      (card: { id: number; text: string }, index: number) => {
+        return (
+          <Card
+            key={card.id}
+            index={index}
+            id={card.id}
+            text={card.text}
+            moveCard={moveCard}
+          />
+        )
+      },
+      [],
+    )
+
+    return (
+      <><div>
+        <h2>{checkTarget(cards) ? "DONE" : ""}</h2>
+        <div className={styles.grid}>
+          {cards.map((card, i) => renderCard(card, i))}
+          </div>
+          </div>
+      </>
+    )
+}
